c9d8c1953df7551c766ef94fae929f11fdceb46b
Glacamora <glacamora@mail.ru>
2018-07-12 22:49:53 +0300

    Ползунок слайдера Живого примера для моб версии + дополнение в функцию вычисления размера окна браузера

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,6 +5,17 @@ window.addEventListener("resize", ready);
 
 function ready() {
   var w = document.documentElement.clientWidth;
+
+  if (w < 768) {
+    w = w - (20 * 2);
+  }
+  else if (w < 1440) {
+    w = w - (30 * 2);
+  }
+  else {
+    w = w - (70 * 2);
+  }
+
   var cat = document.querySelector(".example__img_before");
 
   cat.style.width = w + "px";
